e31f143c2cd2e0f4d93ac3cabe7928c0a7604f68
Entoni1122 <entonishkalla34@gmail.com>
2024-01-16 15:56:11 +0100

    Initial commit

---
+++ dictionary.c
@@ -0,0 +1,64 @@
+#include <stddef.h>
+#include <string.h>
+#include <stdio.h>
+
+#define HASHMAP_SIZE 64
+#define HASHMAP_SIZE_LIST 1
+
+struct aiv_dictionary_item
+{
+    const char *key;
+    const char *value;
+    size_t key_len;
+};
+
+struct aiv_dictionary
+{
+    struct aiv_dictionary_item hashmap[HASHMAP_SIZE][HASHMAP_SIZE_LIST];
+};
+
+size_t djb33x_hash(const char *key, const size_t keylen)
+{
+    size_t hash = 5381;
+
+    for (size_t i = 0; i < keylen; i++)
+    {
+        hash = ((hash << 5) + hash) ^ key[i];
+    }
+
+    return hash;
+}
+
+void aiv_dictionary_insert(struct aiv_dictionary *dictionary, const char *key,const char *value)
+{
+    const size_t key_len = strlen(key);
+
+    const size_t hash = djb33x_hash(key, key_len);
+
+    const size_t index = hash % HASHMAP_SIZE;
+
+    printf("hash of %s = %llu (index: %llu)\n", key, hash, index);
+
+    for (size_t i = 0; i < HASHMAP_SIZE_LIST; i++)
+    {
+        if (dictionary->hashmap[index][i].key_len == 0)
+        {
+            dictionary->hashmap[index][i].key = key;
+            dictionary->hashmap[index][i].value = value;
+            dictionary->hashmap[index][i].key_len = key_len;
+            printf("added %s at index %llu slot %llu\n", key, index, i);
+            return;
+        }
+    }
+
+    printf("COLLISION! for %s with value %s (index %llu)\n", key, value, index);
+}
+
+int main(int argc, char **argv)
+{
+    struct aiv_dictionary myset;
+    
+    aiv_dictionary_insert(&myset,"porcido","dio");
+
+    return 0;
+}
---
+++ linked_list.c
@@ -0,0 +1,210 @@
+#include <stddef.h>
+#include <stdio.h>
+
+#define get_list(x)&(x.list_item)
+
+struct list_item* listPop(struct list_item** head);
+struct list_item* list_append(struct list_item** head, struct list_item* item);
+
+ struct list_item  
+{
+    struct list_item* next;
+    unsigned int counter;
+
+};
+
+struct int_item
+{
+    struct list_item list_item;
+    int value;
+};
+
+struct list_item* list_get_tail(struct list_item* head)
+{
+    if(!head)
+    {
+        return NULL;
+    }
+    struct list_item *current_node = head;
+    struct list_item *last_node = NULL;
+    while (current_node)
+    {
+        last_node = current_node;
+        current_node = current_node->next;
+    }
+        return last_node;
+};
+
+struct list_item* list_append(struct list_item** head, struct list_item* item)
+{
+    struct list_item* tail = list_get_tail(*head);
+    if(!tail)
+    {
+        *head = item;
+    }
+    else
+    {
+        tail->next = item;
+    }
+    item->next = NULL;
+    return item;
+}
+
+unsigned int list_lenght(struct list_item* head)
+{
+    return head->counter;
+}
+
+void list_remove_at(struct list_item** head, int index)
+{
+    unsigned int len = list_lenght(*head);
+
+    if (!*head) 
+    {
+        printf("La lista è vuota\n");
+    }
+
+    if (index == 0) 
+    {
+        printf("Next Time call pop\n");
+        listPop(head);
+        return;
+    }
+
+    struct list_item* current = *head;
+    struct list_item* previous = NULL;
+    int i = 0;
+    if(index > len)
+    {
+        printf("Indice fuori dai limiti della lista\n");
+        return;
+    }
+    while(i < index) 
+    {
+        previous = current;
+        current = current->next;
+        i++;
+    }
+    previous->next = current->next;
+}
+
+struct list_item* list_remove_specific(struct list_item** head,struct list_item* target)
+{
+    unsigned int len = list_lenght(*head);
+    
+    struct list_item* current = *head;
+    struct list_item* prev = NULL;
+    
+    if(len != 0 || target < len)
+    {
+        if(current == target)
+        {
+            listPop(head);
+            return NULL;
+        }
+        for (int i = 0; i < len; i++)
+        {
+            prev = current;
+            current =  current->next;
+            if(current == target)
+            {
+                prev->next = current->next;
+                current = NULL;
+                break;
+            }
+        }
+        return *head;
+    }
+    else
+    {
+        return NULL;
+    }
+}
+
+void printList(struct int_item* head) 
+{
+    while (head != NULL) 
+    {
+        printf("List Value : %d\n", head->value);
+        head = head->list_item.next;
+    }
+}
+
+struct list_item* listPop(struct list_item** head)
+{
+    if(!*head)
+    {
+        return NULL;
+    }
+    struct list_item* current_head = *head;
+    const unsigned int  headCounter = current_head->counter;
+    *head = (*head)->next;
+    if(*head)
+    {
+        (*head)->counter = headCounter - 1;
+    }
+    current_head->next = NULL;
+    return current_head;
+}
+
+void reverse_list(struct list_item** _head)
+{
+    struct  list_item* current = *_head;
+    struct  list_item* next = NULL;
+    struct  list_item* prev = NULL;
+
+    while(!current)
+    {
+        next = current->next;
+        current->next = prev;
+        prev = current;
+        current = next;
+    }
+    (*_head) = prev;
+}
+
+int main(int argc, char** argv)
+{
+    struct list_item* head = NULL;
+    struct int_item item;
+    struct int_item item1;
+    struct int_item item2;
+    struct int_item item3;
+    struct int_item item4;
+    struct int_item item5;
+    
+    item.value = 100;
+    item1.value = 111;
+    item2.value = 122;
+    item3.value = 133;
+    item4.value = 144;
+    item5.value = 155;
+    
+    list_append(&head,(struct list_item *)get_list(item));
+    list_append(&head,(struct list_item *)get_list(item1));
+    list_append(&head,(struct list_item *)get_list(item2));
+    list_append(&head,(struct list_item *)get_list(item3));
+    list_append(&head,(struct list_item *)get_list(item4));
+    list_append(&head,(struct list_item *)get_list(item5));
+
+    printf("-----------List-----------------\n");
+
+    printList(head);
+
+    printf("-----------Remove at specific-----------------\n");
+
+    list_remove_specific(&head,get_list(item3));
+    printList(head);
+    
+    printf("-----------Remove at index-----------------\n");
+
+    list_remove_at(&head,2);
+    printList(head);
+
+    printf("-----------Reverse--------------------\n");
+
+    reverse_list(&head);
+    printList(head);
+
+    return 0;
+}
